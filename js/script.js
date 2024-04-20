c56cf6b1983167f69e0d1ed9b83a962a127a79e5
Shahd Hantoli <134221957+shahdhantoli@users.noreply.github.com>
2024-04-20 23:13:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("signInButton").addEventListener("click", function() {
+    // Code to handle sign-in button click
+    console.log("Sign-in button clicked");
+});
+
+document.getElementById("registerButton").addEventListener("click", function() {
+    // Code to handle register button click
+    console.log("Register button clicked");
+});
